8a1617fe18d366d48722c7b2f971ee74efff9d52
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2018-09-22 03:17:05 -0500

    Finish setting up scene for Cereal Box 1

---
+++ Assets/Scripts/BeeAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeAnimationController : MonoBehaviour {
+
+    public string IsFlyingParameterName = "IsFlying";
+
+    private Animator BeeAnimator;
+
+	// Use this for initialization
+	void Start () {
+        BeeAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetIsFlying(int shouldFly) {
+        BeeAnimator.SetBool(IsFlyingParameterName, shouldFly == 1);
+    }
+}
