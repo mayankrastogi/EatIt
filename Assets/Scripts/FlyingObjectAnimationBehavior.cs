9f48209c4da1a30989756397d24ed9e802657d92
Mayank K Rastogi <mayankrastogi.is@gmail.com>
2018-09-22 21:43:07 -0500

    Finish setting up scene for Cereal Box 2

---
+++ Assets/Scripts/FlyingObjectAnimationBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingObjectAnimationBehavior : MonoBehaviour {
+
+    public string IsFlyingParameterName = "IsFlying";
+
+    private Animator FlyingObjectAnimator;
+
+	// Use this for initialization
+	void Start () {
+        FlyingObjectAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetIsFlying(int shouldFly) {
+        FlyingObjectAnimator.SetBool(IsFlyingParameterName, shouldFly == 1);
+    }
+}
